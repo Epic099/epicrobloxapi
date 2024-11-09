03a87965456ee6d04ae0e2c72d48ea36f90feb90
Epic099Scripting <Speedyben09991@gmail.com>
2024-11-09 13:06:31 +0100

    added crsftoken to post request

--- src/js/base.js
+++ src/js/base.js
@@ -3,6 +3,12 @@ hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 const adduniverse = document.getElementById("add_universe");
 adduniverse.addEventListener("click", function () {
   const input = prompt("Enter universe id")
@@ -12,7 +18,7 @@ adduniverse.addEventListener("click", function () {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({universe_id: input}),
+      body: JSON.stringify({universe_id: input, crsf_token: getCookie('csrftoken')}),
     });
     window.location.href = `/universe/${input}`;
   }
