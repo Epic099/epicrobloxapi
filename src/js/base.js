697f60393a6bad968909de50bd82fd2ae11f837c
Epic099Scripting <Speedyben09991@gmail.com>
2024-11-09 08:50:26 +0100

    Update base.js

--- src/js/base.js
+++ src/js/base.js
@@ -2,3 +2,8 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+const adduniverse = document.getElementById("add_universe");
+adduniverse.addEventListener("click", function () {
+  const input = prompt("Enter universe id")
+});
